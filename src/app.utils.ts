576a314c90cfd83385a45f0e3e369bd23343bd56
willydot21 <willyusername.getall@gmail.com>
2022-07-17 00:00:59 +0200

    fix origin errors

--- src/app.utils.ts
+++ src/app.utils.ts
@@ -1,15 +1,12 @@
 import { VerifyOriginFn } from "../@types/app.utils";
 
-const whiteList:string[] = ['http://localhost:3000'];
+const whiteList:string[] = ['http://localhost:3001', 'https://kinghub-anime.netlify.app'];
 
 export const verifyOrigin:VerifyOriginFn = (origin, callback) => {
 
-  if (!origin) return callback(null, true);
+  if (whiteList.indexOf(origin || '') === -1 || !origin) {
 
-  if (whiteList.indexOf(origin) === -1) {
-
-    var msg = 'The CORS policy for this site does not ' +
-    'allow access from the specified Origin.';
+    var msg = 'The CORS policy for this site does not allow access from the specified Origin.';
 
     return callback(new Error(msg), false);
 
